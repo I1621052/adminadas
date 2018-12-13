37b648cd9494f4a81239f0a1b3b19169a0b59ce4
I1621052 <i1621052@continentla.edu.pe>
2018-12-13 17:37:18 -0500

    modificaciones modulos

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,23 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+
+const PAGESROUTES: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graficas1', component: Graficas1Component },
+            { path: 'dashboard', component: DashboardComponent },
+            { path:'', redirectTo:'/dashboard', pathMatch: 'full'}
+        ]
+    }
+];
+
+export const PAGES_ROUTES = RouterModule.forChild( PAGESROUTES );
