af5993f5b67ee1605785c11ecfc4d05a140298d7
I1621052 <i1621052@continental.edu.pe>
2019-01-01 13:24:58 -0500

    avance

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Habitacion } from '../../models/habitacion.model';
+import { HabitacionService } from '../../services/habitacion/habitacion.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  habitaciones: Habitacion[] = [];
+  desde: number = 0;
+  totalRegitros: number = 0;
+  constructor(
+    public _habitacionService: HabitacionService
+  ) { }
 
   ngOnInit() {
+    this.cargarHabitacion();
   }
-
+  cargarHabitacion() {
+    this._habitacionService.cargarHabitaciones(this.desde)
+      .subscribe(habitaciones => this.habitaciones = habitaciones);
+  }
 }
